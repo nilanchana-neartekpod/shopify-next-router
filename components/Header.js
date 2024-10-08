43e580f3fb224f8e74091f513fb93e73c8e8b7ef
naveenats <naveenats@liveareacx.com>
2024-10-08 14:35:32 +0530

    cart updated

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
+  let [cartId, setCartId] = useState(null);
+
+  useEffect(() =>{
+    let _cartId = sessionStorage.getItem("cartId");
+    if(_cartId) setCartId(_cartId);
+  },[cartId]);
+  
   return (
-    <div>Header</div>
+    <div>
+      Header 
+      {cartId ? (<>
+        <a href={`/cart?cartid=${cartId}`}>View Cart</a>
+      </>) : (<>
+        <a href="/cart">View Cart</a>
+      </>) }
+    </div>
   )
 }
 
