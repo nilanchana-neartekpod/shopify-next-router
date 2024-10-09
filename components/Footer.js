88ddf6bf7315dfde115185564ed874bb95657812
naveenats <naveenats@liveareacx.com>
2024-10-09 14:26:18 +0530

    Modal Updates

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Footer = () => {
+  const [openModal, setOpenModal] = useState();
+
+  const openAssistant = () =>{
+    setOpenModal(true);
+  }
+
+  const closeAssistant = () =>{
+    setOpenModal(false);
+  }
+
   return (
-    <div className='site-footer'>Footer</div>
+    <div className='site-footer'>
+      <button onClick={openAssistant}>Open Voice Assistant</button>
+      <div className={`voice-assistant-modal ${openModal ? 'active': ''}`}>
+        Welcome to voice Assistant
+        <button onClick={closeAssistant}>CloseModal</button>
+      </div>
+    </div>
   )
 }
 
