527bbaa672b5ba0c3d7b8ce6b15c6720126bf770
naveenats <naveenats@liveareacx.com>
2024-10-12 02:51:22 +0530

    PLP updates

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { getProducts } from "../../utils/shopify";
+import ProductCard from "@/components/ProductCard";
 
 const Product = ({data}) => {
+  const products = data.products.nodes;
   return (
-    <div className='mt-24'>Product</div>
+    <div className='mt-20'>
+        <h2 className="text-xl md:text-2xl text-center mt-24 md:mt-32 mb-0">Our Products</h2>
+        <div className="productsList px-4 md:px-12 py-8 md:py-12">
+          {products.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
+        </div>
+    </div>
   )
 }
 
@@ -12,6 +21,6 @@ export const getServerSideProps = async () => {
     return {
       props: { data }, 
     };
-  };
+};
 
 export default Product
