05587b6a0ed3f284ac4b4ba49944ca61b33b1a92
NaveenaTsLivearea <68386068+NaveenaTsLivearea@users.noreply.github.com>
2024-10-12 13:30:55 +0530

    Create Banner.js
    New banner added

---
+++ components/home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='home-banner'>
+        <div className='inner'>
+            <div className='content'>
+                <span>New Arrival</span>
+                <h1>Discover Our New Collection</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+                <a className='text-sm uppercase text-white bg-[#B88E2F] font-bold px-12 py-4 inline-flex' href='/'>BUY Now</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
